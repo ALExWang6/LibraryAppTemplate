7b77de551f59a5ab981870ce51f0d887512fe5ee
kat323 <36419477+kat323@users.noreply.github.com>
2018-03-23 12:31:48 -0400

    Update Member.java

--- src/Member.java
+++ src/Member.java
@@ -12,7 +12,7 @@ public class Member {
      * @return
      *          returns the member's age as an int.
      */
-    public static int getAge() {
+    public int getAge() {
         return 2018 - Member.birthYear;
     }
 
@@ -25,7 +25,7 @@ public class Member {
      * @param book
      *          The book to read.
      */
-    public static void readBook(String) {
+    public void readBook(String) {
         System.out.println("I'm reading " + String + "!");
     }
 }
