44b89b4077b9eba2f3439d7b84a99e3c086bd41b
KianTehrani <36419464+KianTehrani@users.noreply.github.com>
2018-03-23 13:11:59 -0400

    Add files via upload

---
+++ LibraryApp.java
@@ -0,0 +1,21 @@
+public class LibraryApp {
+    private static Library library = new Library();
+
+    public static void main(String[] args) {
+        System.out.println(library);
+
+        Book book1 = new Book ();
+        book1.title = "The Lost Cause";
+
+        Book book2 = new Book ();
+        book2.title = "Censored Minds";
+
+        Book book3 = new Book ();
+        book3.title = "Sunday Funday Massacre";
+
+
+    }
+}
+
+
+
