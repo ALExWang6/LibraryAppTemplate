de02d51135182193a7fc884b5c37c783c6f7cb8c
MingzeJin <36276890+MingzeJin@users.noreply.github.com>
2018-03-23 12:59:55 -0400

    Update LibraryApp.java

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -3,5 +3,11 @@ public class LibraryApp {
 
     public static void main(String[] args) {
         System.out.println(library);
+        
+        Member.member1 = new Member();
+        member1.name = "Mingze Jin";
+        member1.birthYear = 2000;
+        member1.address = "23 Sycamore Dr";
+        
     }
 }
